b965cd7aba808f6e65732dc98067ba2f2aeaea37
TMiguelT <michael.r.milton@gmail.com>
2015-04-09 16:59:31 +1000

    Finished tests, documentation

---
+++ readme.md
@@ -0,0 +1,86 @@
+# koa-pg-session
+
+## Introduction
+
+koa-pg-session is an implementation of a session store for koa's [generic session module](https://github.com/koajs/generic-session) that uses Postgres to hold the session data.
+
+This module is ideal if you're already using Postgres as the database layer for your app, and you want to add a session store without adding another dependency like Redis.
+
+## Usage
+
+Install the module and the generic session module using `npm install koa-generic-session koa-pg-session`
+
+Require these modules in your main koa module:
+
+```javascript
+var session = require('koa-generic-session');
+var PgStore = require('koa-pg-session');
+```
+
+Use the session somewhere in your middleware:
+
+```javascript
+var app = require('koa')();
+app.use(session({
+    store: new PgStore("postgres://username:password@localhost/database")
+}))
+```
+
+## Database Table
+
+By default, the module will create a new table in the database called 'session', located in the public schema. This table has the schema:
+
+```sql
+CREATE TABLE IF NOT EXISTS <tablename>
+    id TEXT NOT NULL PRIMARY KEY,
+    expiry timestamp NOT NULL,
+    session JSON
+);
+```
+
+If a table with this name already exists, it will be connected to, and the module will assume that these three columns exist in the table, and will fail if they do not. However you can create a table with additional columns without issue.
+
+You can also change the name of the table as explained in the [constructor](#constructor) section, meaning that the module will either create a table with a different name/schema, or it will use an already existing table with this name/schema.
+
+## API
+
+### Constructor
+
+The koa-pg-session module returns a constructor function that takes two parameters.
+
+```javascript
+var PgStore = require('koa-pg-session');
+new PgStore(connection, options);
+```
+
+The first parameter, `connection`, is a connection object or connect string that will be passed *directly* into the pg module. As of the writing of this, `connection` can either be a connection string, e.g. `"postgres://username:password@localhost/database"`, or it can be a connection object, e.g.
+```javascript
+{
+      user: 'brianc',
+      password: 'boom!',
+      database: 'test',
+      host: 'example.com',
+      port: 5313
+}
+```
+
+For further information, see the [pg module's documentation](https://github.com/brianc/node-postgres/wiki/pg#connectstring-connectionstring-function-callback).
+
+The second parameter, `options`, is an object consisting of all optional keys.
+
+`schema` String. defines the schema in which to create or find the table that we will use to store session data. Defaults to `public`
+`table` String. defines the name of the sessions table that we will create or find. Defaults to `session`
+`create` Boolean. True if the module is allowed to create a new table to store sessions. Defaults to `true`
+`cleanupTime` Number, in milliseconds. The amount of time between cleaning up the database for old sessions. Defaults to 162000000 (45 minutes)
+
+### koa-generic-session options
+
+Most of the customisation for this module is available through the options passed into the koa-generic-session module, which are [listed here](https://github.com/koajs/generic-session#options), for example we can allow empty sessions by using this middleware:
+
+```javascript
+app.use(session({
+    store: new PgStore("postgres://username:password@localhost/database"),
+    allowEmpty: true
+}))
+```
+
--- test/test.js
+++ test/test.js
@@ -1,52 +1,269 @@
 var assert = require("assert");
-var sinon = require("sinon");
+var sinon = require("sinon-es6");
 var Promise = require('bluebird');
+var proxyquire = require('proxyquire');
 var PgSession = require("../index");
+var co = require('co');
+require('co-mocha');
+
+
+//Sandbox everything always
+var sandbox;
+beforeEach(function () {
+    sandbox = sinon.sandbox.create();
+});
+
+afterEach(function () {
+    sandbox.restore();
+});
 
 describe('PgSession constructor', function () {
 
-    //Sandbox everything
-    var sandbox;
-    beforeEach(function () {
-        sandbox = sinon.sandbox.create();
-    });
-
-    afterEach(function () {
-        sandbox.restore();
-    });
-
-    it('should always start the cleanup process', function (done) {
+    it('should start the cleanup when create=true', function (done) {
 
         //Spy on the cleanup method and make the query a stub that returns a promise
         var cleanup = sandbox.stub(PgSession.prototype, "cleanup");
-        sandbox.stub(PgSession.prototype, "query").returns(Promise.resolve());
+        sandbox.stub(PgSession.prototype, "query").returns(new Promise(function () {
+            done();
+        }));
 
         //Call the constructor with create: true and create: false
         new PgSession("", {create: true});
-        new PgSession("", {create: false});
-
-        setTimeout(function () {
-            assert(cleanup.calledTwice);
-            done();
-        }, 0);
-
     });
 
-    it("should run the create table SQL if create: true", function (done) {
-        var qry = sandbox.stub(PgSession.prototype, "query").returns(Promise.resolve());
+    it('should start the cleanup when create=false', function (done) {
+
+        //Spy on the cleanup method and make the query a stub that returns a promise
+        var cleanup = sandbox.stub(PgSession.prototype, "cleanup");
+
+        //Call the constructor with create: true and create: false
+        new PgSession("", {create: false});
+        assert(cleanup.called);
+        done();
+    });
+
+
+    it("should run the create table SQL if create=true", function (done) {
+        sandbox.stub(PgSession.prototype, "query", function (arg) {
+            if (arg.indexOf("CREATE TABLE") == -1)
+                done(new Error());
+            else
+                done();
+        });
 
         new PgSession("", {create: true});
-
-        setTimeout(function () {
-            assert(qry.called);
-            done();
-        }, 0);
     });
 
 });
 
-//describe('query method', function () {
-//
-//    const pg = require('co-pg')(require('pg'));
-//
-//});
+describe('#query method', function () {
+
+    const pg = require('co-pg')(require('pg'));
+    var opts = {
+        some: "var",
+        other: "value"
+    };
+    var fakeSession = {
+        options: opts
+    };
+
+    it("should connect to the database with the given input options", function () {
+        sandbox.mock(pg).expects("connectPromise").once().withArgs(opts);
+        PgSession.prototype.query.call(fakeSession);
+    });
+
+    it("should query with the values passed in", function *(done) {
+        var sql = "SOME SQL";
+        var params = [1, "Fred"];
+        var stub = sinon.stub().returns(Promise.resolve());
+
+        var MockedPgSession = proxyquire("../index", {
+            "co-pg": function (pg) {
+                //Add the connectPromise method
+                pg = require('co-pg')(pg);
+
+                //Then stub it
+                sandbox.stub(pg, "connectPromise").returns(Promise.resolve(
+                    [
+                        {
+                            queryPromise: stub
+                        },
+                        sinon.stub()
+                    ]));
+
+                return pg;
+            }
+        });
+
+        yield MockedPgSession.prototype.query.call(fakeSession, sql, params);
+
+        try {
+            assert(stub.calledOnce && stub.alwaysCalledWith(sql, params));
+        }
+        catch (e) {
+            done(e)
+        }
+
+        done();
+
+    });
+});
+
+describe("#get method", function (done) {
+
+    it("should call #query", function *(done) {
+        var proxy = {
+            getValueSql: "",
+            query: function () {
+                done();
+            }
+        };
+
+        yield PgSession.prototype.get.call(proxy);
+    });
+
+    it("should return false if no rows were returned", function *(done) {
+        var proxy = {
+            getValueSql: "",
+            query: function *() {
+                return {rows: []};
+            }
+        };
+
+        var res = yield PgSession.prototype.get.call(proxy);
+        if (res === false)
+            done();
+        else
+            done(new Error());
+    })
+});
+
+describe("#set method", function () {
+    var update = "UPDATE";
+    var insert = "INSERT";
+    var fakeSession = {
+        updateValueSql: update,
+        insertValueSql: insert
+    };
+
+    it("should update an existing row if it exists", function *(done) {
+        //Pretend that the row already exists
+        fakeSession.get = function () {
+            return Promise.resolve(true);
+        };
+
+        //When query is run, make sure it's with the right SQL
+        fakeSession.query = function (sql) {
+            if (sql === update)
+                done();
+            else
+                done(new Error());
+        };
+
+        yield PgSession.prototype.set.call(fakeSession);
+    });
+
+    it("should insert a existing row if it doesn't already exist", function *(done) {
+        //Pretend that the row doesn't already exist
+        fakeSession.get = function () {
+            return Promise.resolve(false);
+        };
+
+        //When query is run, make sure it's with the right SQL
+        fakeSession.query = function (sql) {
+            if (sql === insert)
+                done();
+            else
+                done(new Error());
+        };
+
+        yield PgSession.prototype.set.call(fakeSession);
+    });
+});
+
+describe("#destroy method", function () {
+    it("calls #query with an SQL DELETE statement", function *(done) {
+        var del = "DELETE";
+        var fakeSession = {
+            destroyValueSql: del,
+            query: function (sql) {
+                if (sql === del)
+                    done();
+                else
+                    done(new Error());
+            }
+        };
+
+        yield PgSession.prototype.destroy.call(fakeSession);
+    });
+});
+
+describe("#cleanup method", function () {
+    it("calls #query after the cleanupTime interval", function *(done) {
+        var time = 2;
+        var cleanup = "DELETE";
+        var spy = sinon.spy();
+
+        var fakeSession = {
+            options: {
+                cleanupTime: time
+            },
+            query: spy,
+            cleanupSql: cleanup
+        };
+
+        setTimeout(function () {
+            if (spy.called && spy.alwaysCalledWith(cleanup))
+                done();
+            else
+                done(new Error());
+        }, time * 2);
+
+        yield PgSession.prototype.cleanup.call(fakeSession);
+    });
+});
+
+describe("SQL Properties", function () {
+
+    var fakeSession = Object.create(PgSession.prototype);
+    fakeSession.options = {
+        schema: "public",
+        table: "session"
+    };
+
+    describe("createSql property", function () {
+        it("needs to have a CREATE TABLE statement", function () {
+            assert(fakeSession.createSql.indexOf("CREATE TABLE") != -1);
+        });
+    });
+
+    describe("getValueSql property", function () {
+        it("needs to have a SELECT statement", function () {
+            assert(fakeSession.getValueSql.indexOf("SELECT") != -1);
+        });
+    });
+
+    describe("updateValueSql property", function () {
+        it("needs to have an UPDATE statement", function () {
+            assert(fakeSession.updateValueSql.indexOf("UPDATE") != -1);
+        });
+    });
+
+    describe("insertValueSql property", function () {
+        it("needs to have an INSERT statement", function () {
+            assert(fakeSession.insertValueSql.indexOf("INSERT") != -1);
+        });
+    });
+
+    describe("destroyValueSql property", function () {
+        it("needs to have a DELETE statement", function () {
+            assert(fakeSession.destroyValueSql.indexOf("DELETE FROM") != -1);
+        });
+    });
+
+    describe("cleanupSql property", function () {
+        it("needs to have a DELETE statement", function () {
+            assert(fakeSession.cleanupSql.indexOf("DELETE FROM") != -1);
+        });
+    });
+});
