49dbfd29a201c2cec86de071a9d53051b16dd8e3
Michael Milton <ttmigueltt@gmail.com>
2015-11-22 01:17:42 +0100

    Converted to ES6 class

--- index.js
+++ index.js
@@ -14,111 +14,115 @@ const defaultOpts = {
     cleanupTime: ms('45 minutes')
 };
 
-/**
- * Creates a new PgSession model for use with koa-session-generic
- * @param connection The connection string or object to be passed directly into the pg module
- * @param options A hash consisting of all optional keys {schema="public", table="session", create=true, cleanupTime = 45 minutes}
- * @constructor
- */
-function PgSession(connection, options) {
+module.exports = class PgSession {
 
-    //Create a PG client and store it in the object
-    this.db= pgp(connection);
+    /**
+     * Creates a new PgSession model for use with koa-session-generic
+     * @param connection The connection string or object to be passed directly into the pg module
+     * @param options A hash consisting of all optional keys {schema="public", table="session", create=true, cleanupTime = 45 minutes}
+     * @constructor
+     */
+    constructor(connection, options) {
 
-    //And store the session options
-    this.options = Object.assign({}, defaultOpts, options);
-}
+        //Create a PG client and store it in the object
+        this.db = pgp(connection);
 
-/**
- * Starts the cleanup and creates the session tables if necessary
- * @returns A promise that resolves when the setup has completed
- */
-PgSession.prototype.setup = function () {
+        //And store the session options
+        this.options = Object.assign({}, defaultOpts, options);
+    }
 
-    let sess = this;
+    /**
+     * Starts the cleanup and creates the session tables if necessary
+     * @returns A promise that resolves when the setup has completed
+     */
+    setup() {
 
-    //If we need to create the tables, return a promise that resolves once the query completes
-    if (this.options.create) {
+        let sess = this;
 
-        return Promise.coroutine(function *() {
-            return yield sess.db.query(sess.createSql);
-            //Then set up the table cleanup
-        })().then(function () {
+        //If we need to create the tables, return a promise that resolves once the query completes
+        if (this.options.create) {
+
+            return Promise.coroutine(function *() {
+                return yield sess.db.query(sess.createSql);
+                //Then set up the table cleanup
+            })().then(function () {
+                sess.cleanup();
+            });
+
+        }
+
+        //Otherwise just setup the cleanup and return an empty promise
+        else {
             sess.cleanup();
-        });
+            return Promise.resolve();
+        }
+    };
 
-    }
+    /**
+     * Gets a session object with the given sid
+     * @param sid The Koa session ID
+     * @returns The session object if it exists, otherwise false
+     */
 
-    //Otherwise just setup the cleanup and return an empty promise
-    else {
-        sess.cleanup();
-        return Promise.resolve();
-    }
-};
+    *get(sid) {
 
-/**
- * Gets a session object with the given sid
- * @param sid The Koa session ID
- * @returns The session object if it exists, otherwise false
- */
-PgSession.prototype.get = function *(sid) {
+        //Get the existing session row
+        const existing = (yield this.db.query(this.getValueSql, [sid])).rows;
 
-    //Get the existing session row
-    const existing = (yield this.db.query(this.getValueSql, [sid])).rows;
+        //If there is no such row, return false
+        if (existing.length <= 0)
+            return false;
+        //Otherwise return the row
+        else
+            return existing[0].session;
+    };
 
-    //If there is no such row, return false
-    if (existing.length <= 0)
-        return false;
-    //Otherwise return the row
-    else
-        return existing[0].session;
-};
+    /**
+     * Creates a new session or updates an existing one
+     * @param sid The Koa session ID to set
+     * @param sess The session date to insert into the session table
+     * @param ttl The time to live, i.e. the time until the session expires
+     */
 
-/**
- * Creates a new session or updates an existing one
- * @param sid The Koa session ID to set
- * @param sess The session date to insert into the session table
- * @param ttl The time to live, i.e. the time until the session expires
- */
-PgSession.prototype.set = function *(sid, sess, ttl) {
+    *set(sid, sess, ttl) {
 
-    //If there is a row, update it
-    if (yield this.get(sid))
-        yield this.db.query(this.updateValueSql, [sess, ttl, sid]);
+        //If there is a row, update it
+        if (yield this.get(sid))
+            yield this.db.query(this.updateValueSql, [sess, ttl, sid]);
 
-    //Otherwise, insert a new row
-    //(These two queries intentionally have a different parameter order because of the SQL structure)
-    else
-        yield this.db.query(this.insertValueSql, [sid, sess, ttl]);
-};
+        //Otherwise, insert a new row
+        //(These two queries intentionally have a different parameter order because of the SQL structure)
+        else
+            yield this.db.query(this.insertValueSql, [sid, sess, ttl]);
+    };
 
-/**
- * Destroy the session with the given sid
- * @param sid The Koa session ID of the session to destroy
- */
-PgSession.prototype.destroy = function *(sid) {
-    yield this.db.query(this.destroyValueSql, [sid]);
-};
+    /**
+     * Destroy the session with the given sid
+     * @param sid The Koa session ID of the session to destroy
+     */
+    *destroy(sid) {
+        yield this.db.query(this.destroyValueSql, [sid]);
+    };
 
-/**
- * Setup cleanup of all sessions in the session table that have expired
- */
-PgSession.prototype.cleanup = function () {
-    let sess = this;
+    /**
+     * Setup cleanup of all sessions in the session table that have expired
+     */
+    cleanup() {
+        let sess = this;
 
-    //Each interval of cleanupTime, run the cleanup script
-    setInterval(function () {
-        Promise.coroutine(function *() {
-            yield sess.db.query(sess.cleanupSql);
-        });
-    }, sess.options.cleanupTime);
-};
+        //Each interval of cleanupTime, run the cleanup script
+        setInterval(function () {
+            Promise.coroutine(function *() {
+                yield sess.db.query(sess.cleanupSql);
+            });
+        }, sess.options.cleanupTime);
+    };
 
-/**
- * Get the raw SQL for creating a new session table
- */
-Object.defineProperty(PgSession.prototype, "createSql", {
-    get: function myProperty() {
+    /**
+     * Get the raw SQL for creating a new session table
+     */
+
+    get createSql() {
         return escape(
             'CREATE SCHEMA IF NOT EXISTS %I;\n' +
             'CREATE TABLE IF NOT EXISTS %I.%I (\n' +
@@ -131,72 +135,59 @@ Object.defineProperty(PgSession.prototype, "createSql", {
             this.options.table
         );
     }
-});
 
-/**
- * Get the raw SQL for getting an existing session
- */
-Object.defineProperty(PgSession.prototype, "getValueSql", {
-    get: function myProperty() {
+    /**
+     * Get the raw SQL for getting an existing session
+     */
+    get getValueSql() {
         return escape(
             'SELECT session FROM %I.%I WHERE id = $1;',
             this.options.schema,
             this.options.table
         );
     }
-});
 
-/**
- * Get the raw SQL for updating an existing session
- */
-Object.defineProperty(PgSession.prototype, "updateValueSql", {
-    get: function myProperty() {
+    /**
+     * Get the raw SQL for updating an existing session
+     */
+    get updateValueSql() {
         return escape(
             "UPDATE %I.%I SET session = $1, expiry = (now() + $2 * interval '1 ms') WHERE id = $3;",
             this.options.schema,
             this.options.table
         );
     }
-});
 
-/**
- * Get the raw SQL for creating a new existing session
- */
-Object.defineProperty(PgSession.prototype, "insertValueSql", {
-    get: function myProperty() {
+    /**
+     * Get the raw SQL for creating a new existing session
+     */
+    get insertValueSql() {
         return escape(
             "INSERT INTO %I.%I(id, session, expiry) VALUES($1, $2, (now() + $3 * interval '1 ms'));",
             this.options.schema,
             this.options.table
         );
     }
-});
 
-/**
- * Get the raw SQL for destroying an existing session
- */
-Object.defineProperty(PgSession.prototype, "destroyValueSql", {
-    get: function myProperty() {
+    /**
+     * Get the raw SQL for destroying an existing session
+     */
+    get destroyValueSql() {
         return escape(
             'DELETE FROM %I.%I WHERE id = $1;',
             this.options.schema,
             this.options.table
         );
     }
-});
 
-/**
- * Get the raw SQL for cleaning up expired sessions
- */
-Object.defineProperty(PgSession.prototype, "cleanupSql", {
-    get: function myProperty() {
+    /**
+     * Get the raw SQL for cleaning up expired sessions
+     */
+    get cleanupSql() {
         return escape(
             'DELETE FROM %I.%I WHERE expiry <= now();',
             this.options.schema,
             this.options.table
         );
     }
-});
-
-//Export the PgSession class since everything is attached to it and its prototype
-module.exports = PgSession;
+}
