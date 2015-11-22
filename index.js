8db4b930abbaf4f1c39e478fcc33b148f0b7853c
Michael Milton <ttmigueltt@gmail.com>
2015-11-22 06:52:44 +0100

    Rewrote tests, added custom client option

--- index.js
+++ index.js
@@ -6,56 +6,65 @@ const escape = require('pg-escape');
 const ms = require('ms');
 const Promise = require('bluebird');
 
-//Constants
-const defaultOpts = {
-    schema: "public",
-    table: "session",
-    create: true, //Create a new session table by default
-    cleanupTime: ms('45 minutes')
-};
-
 module.exports = class PgSession {
 
     /**
      * Creates a new PgSession model for use with koa-session-generic
      * @param connection The connection string or object to be passed directly into the pg module
      * @param options A hash consisting of all optional keys {schema="public", table="session", create=true, cleanupTime = 45 minutes}
+     * @param customClient A boolean, true if they want to pass in their own client, otherwise false (false by default)
      * @constructor
      */
-    constructor(connection, options) {
+    constructor(connection, options, customClient) {
 
-        //Create a PG client and store it in the object
-        this.db = pgp(connection);
+        //If they want to use an existing client, they set customClient to true and pass in a function to
+        //process each query. Their function must return a promise.
+        if (customClient) {
+            if (typeof connection != "function")
+                throw new Error("connection must be a function if customClient is true!");
+            this.query = connection;
+        }
+
+        //If they don't want to use an existing client, make our own connection to the database and use that for queries
+        else {
+            this.db = pgp(connection);
+            this.query = (query, params)=> {
+                return this.db.query(query, params);
+            }
+        }
 
         //And store the session options
-        this.options = Object.assign({}, defaultOpts, options);
+        this.options = Object.assign({}, PgSession.defaultOpts, options);
+    }
+
+    static get defaultOpts() {
+        return {
+            schema: "public",
+            table: "session",
+            create: true, //Create a new session table by default
+            cleanupTime: null
+        };
     }
 
     /**
-     * Starts the cleanup and creates the session tables if necessary
-     * @returns A promise that resolves when the setup has completed
+     * Creates the session table if necessary
+     * @returns {*} A promise that resolves when the setup has completed
      */
     setup() {
 
         let sess = this;
 
+        //Note that we're no longer startup the cleanup here. Rather this happens after the first
+        //insertion/session creation
+
         //If we need to create the tables, return a promise that resolves once the query completes
         if (this.options.create) {
-
-            return Promise.coroutine(function *() {
-                return yield sess.db.query(sess.createSql);
-                //Then set up the table cleanup
-            })().then(function () {
-                sess.cleanup();
-            });
-
+            return sess.query(sess.createSql);
         }
 
         //Otherwise just setup the cleanup and return an empty promise
-        else {
-            sess.cleanup();
+        else
             return Promise.resolve();
-        }
     };
 
     /**
@@ -67,7 +76,7 @@ module.exports = class PgSession {
     *get(sid) {
 
         //Get the existing session row
-        const existing = (yield this.db.query(this.getValueSql, [sid])).rows;
+        const existing = (yield this.query(this.getValueSql, [sid]));
 
         //If there is no such row, return false
         if (existing.length <= 0)
@@ -81,19 +90,32 @@ module.exports = class PgSession {
      * Creates a new session or updates an existing one
      * @param sid The Koa session ID to set
      * @param sess The session date to insert into the session table
-     * @param ttl The time to live, i.e. the time until the session expires
+     * @param ttl The time to live, i.e. the time until the session expires. Defaults to 45 minutes
      */
 
     *set(sid, sess, ttl) {
 
+        ttl = ttl || ms("45 minutes");
+        const expiry = (Date.now() + ttl) / 1000;
+
+        //If the ttl is less than our current cleanup interval, decrease the cleanup interval
+        if (ttl < this.options.cleanupTime)
+            this.options.cleanupTime = ttl;
+
+        //Or, if this is the first session we're adding, set the cleanup interval to the TTL, and start the cleanup
+        if (this.options.cleanupTime === null) {
+            this.options.cleanupTime = ttl;
+            this.cleanup();
+        }
+
         //If there is a row, update it
-        if (yield this.get(sid))
-            yield this.db.query(this.updateValueSql, [sess, ttl, sid]);
+        if (yield* this.get(sid))
+            yield this.query(this.updateValueSql, [sess, expiry, sid]);
 
         //Otherwise, insert a new row
         //(These two queries intentionally have a different parameter order because of the SQL structure)
         else
-            yield this.db.query(this.insertValueSql, [sid, sess, ttl]);
+            yield this.query(this.insertValueSql, [sid, sess, expiry]);
     };
 
     /**
@@ -101,7 +123,7 @@ module.exports = class PgSession {
      * @param sid The Koa session ID of the session to destroy
      */
     *destroy(sid) {
-        yield this.db.query(this.destroyValueSql, [sid]);
+        yield this.query(this.destroyValueSql, [sid]);
     };
 
     /**
@@ -111,9 +133,10 @@ module.exports = class PgSession {
         let sess = this;
 
         //Each interval of cleanupTime, run the cleanup script
-        setInterval(function () {
-            Promise.coroutine(function *() {
-                yield sess.db.query(sess.cleanupSql);
+        setTimeout(function interval() {
+            sess.query(sess.cleanupSql, Date.now()).then(()=> {
+                //Recurse so that the cleanupTime can be dynamic
+                setTimeout(interval, sess.options.cleanupTime);
             });
         }, sess.options.cleanupTime);
     };
@@ -152,7 +175,7 @@ module.exports = class PgSession {
      */
     get updateValueSql() {
         return escape(
-            "UPDATE %I.%I SET session = $1, expiry = (now() + $2 * interval '1 ms') WHERE id = $3;",
+            "UPDATE %I.%I SET session = $1, expiry = to_timestamp($2) WHERE id = $3;",
             this.options.schema,
             this.options.table
         );
@@ -163,7 +186,7 @@ module.exports = class PgSession {
      */
     get insertValueSql() {
         return escape(
-            "INSERT INTO %I.%I(id, session, expiry) VALUES($1, $2, (now() + $3 * interval '1 ms'));",
+            "INSERT INTO %I.%I(id, session, expiry) VALUES($1, $2, to_timestamp($3) );",
             this.options.schema,
             this.options.table
         );
@@ -185,9 +208,9 @@ module.exports = class PgSession {
      */
     get cleanupSql() {
         return escape(
-            'DELETE FROM %I.%I WHERE expiry <= now();',
+            'DELETE FROM %I.%I WHERE expiry <= to_timestamp($1);',
             this.options.schema,
             this.options.table
         );
     }
-}
+};
--- test/test.js
+++ test/test.js
@@ -1,9 +1,9 @@
 "use strict";
 
+//Require calls//
+
 require('co-mocha');
 const PgSession = require("../index");
-const Promise = require('bluebird');
-
 /**
  * Load the connection data from a local config file.
  * Whoever is running this test must provide this config file, which must export a JS object/be a JSON file
@@ -17,33 +17,83 @@ const Promise = require('bluebird');
 };
  */
 const connection = require('./config');
-
 const pgp = require('pg-promise')();
 const db = pgp(connection);
+const ms = require('ms');
+const Promise = require('bluebird');
+const assert = require('assert');
+const co = Promise.coroutine;
+
+//Mini server dependencies
+const koa = require('koa');
+const request = require('request-promise');
+const generic = require('koa-generic-session');
+
+//Utility functions//
 
 /**
- * Created by miguel on 21/11/15.
+ * Returns true if the given table exists
+ * @param table An object of the form {schema, table} that refers to a specific table to check
  */
-describe('#setup method', () => {
-
-    /**
-     * Returns true if the given table exists
-     * @param table An object of the form {schema, table} that refers to a specific table to check
-     */
-    function* tableExists(table){
-        return (yield db.query(`
+function* tableExists(table) {
+    return (yield db.query(`
                SELECT COUNT(*) > 0 as exists
                FROM   information_schema.tables
                WHERE  table_schema = $[schema]
                AND    table_name = $[table]
             `, table))[0].exists;
-    }
+}
 
-    //The location to put the test table
-    const sampleTable = {
-        table: 'sampleTable',
-        schema: '__koa_pg_session_test'
-    };
+/**
+ * Deletes the schema containing the given table object
+ * @param tableObject
+ * @returns {*}
+ */
+function deleteSchema(tableObject) {
+    return db.query(`DROP SCHEMA ${tableObject.schema} CASCADE`);
+}
+
+//The location to put the test table
+const sampleTable = {
+    table: 'sampleTable',
+    schema: '__koa_pg_session_test'
+};
+
+//Common Constants//
+const sampleID = "123";
+const values = {a: 1, b: 2};
+
+//Tests//
+
+describe('constructor', ()=> {
+    it('should default to the correct options', () => {
+        assert.deepEqual(new PgSession(connection).options, PgSession.defaultOpts);
+    });
+
+    it('should throw an error if no connection details are given', done=> {
+        try {
+            new PgSession();
+            done(new Error("No exception thrown"))
+        }
+        catch (ex) {
+            done();
+        }
+    });
+});
+
+describe('#setup method', () => {
+
+    it('should return a promise no matter if create=true or false', done => {
+        let noCreate = new PgSession(connection, Object.assign({create: false}, sampleTable)).setup();
+        let create = new PgSession(connection, Object.assign({create: true}, sampleTable)).setup();
+
+        assert(typeof noCreate.then == "function" && typeof create.then == "function");
+        create.then(()=> {
+            return deleteSchema(sampleTable);
+        }).then(()=> {
+            done();
+        });
+    });
 
     it('should create a table and schema with the correct names if create=true', done => {
 
@@ -51,19 +101,16 @@ describe('#setup method', () => {
         let session = new PgSession(connection, Object.assign({create: true}, sampleTable));
 
         //Now wait for it to finish, then test to see if it's in the database
-        Promise.coroutine(function*() {
+        co(function*() {
             if (yield* tableExists(sampleTable))
                 done(new Error("Table already exists: delete it then re-run the test"));
 
             yield session.setup();
 
-            console.log(session.createSql);
-
             //If the table exists, everything's working, but also delete it to clean up
             let exists = yield* tableExists(sampleTable);
-            console.log(`Exists? ${exists}`);
             if (exists) {
-                yield db.query(`DROP SCHEMA ${sampleTable.schema} CASCADE`);
+                yield deleteSchema(sampleTable);
                 done();
             }
             else
@@ -77,18 +124,225 @@ describe('#setup method', () => {
         let session = new PgSession(connection, Object.assign({create: false}, sampleTable));
 
         //Now wait for it to finish, then test to see if it's in the database
-        Promise.coroutine(function*() {
+        co(function*() {
             if (yield* tableExists(sampleTable))
                 done(new Error("Table already exists: delete it then re-run the test"));
 
             yield session.setup();
 
-            //If the table exists, everything's working, but also delete it to clean up
-            if (yield* tableExists(sampleTable)) {
+            //If the table exists, that's bad!
+            if (yield* tableExists(sampleTable))
                 done(new Error("Table was created"));
-            }
             else
                 done();
+        })();
+    });
+});
+
+describe('#get #set, and #destroy (public interface) functions', ()=> {
+    const session = new PgSession(connection, Object.assign({create: true}, sampleTable));
+    const otherValues = {c: 3, d: 4};
+    const expiryTime = ms("45 minutes");
+
+    before(function (done) {
+        //First we have to setup the session
+        session.setup().then(()=> {
+            done();
+        })
+    });
+
+    after(function (done) {
+        //Once we're done, delete the table
+        deleteSchema(sampleTable).then(() => {
+            done();
+        });
+    });
+
+    it('should set and be able to retrieve data', done => {
+        co(function*() {
+
+            //Check that we can create a new session object
+            yield* session.set(sampleID, values, expiryTime);
+            assert.deepEqual(yield* session.get(sampleID), values);
+            done();
+        })();
+    });
+
+    it('should update existing session data', done => {
+        co(function*() {
+
+            //Check that we can update it
+            yield* session.set(sampleID, otherValues, expiryTime);
+            assert.deepEqual(yield* session.get(sampleID), otherValues);
+            done();
+        })();
+    });
+
+    it('should not return a result after deleted', done => {
+        co(function*() {
+
+            //Check that we can destroy it
+            yield* session.destroy(sampleID);
+            assert.deepEqual(yield* session.get(sampleID), false);
+            done();
+        })();
+    });
+});
+
+describe('Automatic cleanup', ()=> {
+    const session = new PgSession(connection, Object.assign({create: true}, sampleTable));
+    const expiryTime = 100;
+
+    before(function (done) {
+        //First we have to setup the session manager
+        session.setup().then(()=> {
+            done();
+        })
+    });
+
+    after(function (done) {
+        //Once we're done, delete the table
+        deleteSchema(sampleTable).then(() => {
+            done();
+        });
+    });
+
+    it("should still exist before the expiry time has passed", function (done) {
+        co(function*() {
+            yield* session.set(sampleID, values, expiryTime);
+
+            //Before the expiry time (at 50ms, when the expiry is 100ms), the session should still be there
+            setTimeout(()=> {
+                co(function*() {
+                    assert((yield* session.get(sampleID)) !== false);
+                    done();
+                })();
+
+            }, 50);
+        })();
+    });
+
+    it("should be deleted after the expiry time has passed", function (done) {
+        co(function*() {
+            yield* session.set(sampleID, values, expiryTime);
+
+            //After the expiry time (at 150ms when the expiry is 100ms), the session should be deleted
+            setTimeout(()=> {
+                co(function*() {
+                    const sess = yield* session.get(sampleID);
+                    assert(sess === false);
+                    done();
+                })();
+            }, 150);
+        })();
+    });
+
+    //Checks that the cleanup function is being called regularly
+    it("should continue to run the cleanup function", function (done) {
+
+        co(function*() {
+
+            //Create the first session
+            yield* session.set(sampleID, values, expiryTime);
+
+            //Every 200ms, check that the old session is deleted, and add a new one
+            let counter = 0;
+            const iid = setInterval(() => {
+
+                //Check the old session has gone
+                co(function*() {
+                    const sess = yield* session.get(sampleID);
+                    assert(sess === false);
+                })();
+
+                //Increment the counter and quit when we're done
+                counter++;
+                if (counter >= 4) {
+                    done();
+                    clearInterval(iid);
+                } else {
+                    //Add a new session if we're not going to quit
+                    co(function*() {
+                        yield* session.set(sampleID, values, expiryTime);
+                    })();
+                }
+            }, 200);
+        })();
+    });
+});
+
+describe("Compatibility with koa and koa-generic sessions", ()=> {
+    const session = new PgSession(connection, Object.assign({create: true}, sampleTable));
+
+    before(function (done) {
+        //First we have to setup the session manager
+        session.setup().then(()=> {
+            done();
+        })
+    });
+
+    after(function (done) {
+        //Once we're done, delete the table
+        deleteSchema(sampleTable).then(() => {
+            done();
+        });
+    });
+
+    it("Should persist data between sessions", function (done) {
+        this.timeout(0);
+
+        //Make a little koa server
+        const app = koa();
+        app.keys = ['keys', 'keykeys'];
+        app.use(generic({
+            store: session
+        }));
+
+        //Whenever we visit the server, update the counter and display the current count
+        app.use(function *() {
+            this.body = this.session.test++;
+        });
+
+        app.listen(3000);
+
+        //Check that the counter is working
+        co(function*() {
+            let opts = {url: 'http://localhost:3000/', jar: true};
+            assert((yield request(opts)) === "null");
+            assert((yield request(opts)) === "0");
+            assert((yield request(opts)) === "1");
+            assert((yield request(opts)) === "2");
+            done();
+        })();
+
+    });
+});
+
+describe('custom client option', ()=> {
+    const pg = require('co-pg')(require('pg'));
+    const client = new pg.Client(connection);
+
+    it('Correctly creates a new table with a custom client', done=> {
+
+        co(function*() {
+            //Connect to the custom client
+            yield client.connectPromise();
+
+            //Make a session manager which uses co-pg instead
+            const session = new PgSession((query, args)=> {
+                return client.queryPromise(query, args)
+            }, Object.assign({create: true}, sampleTable), true);
+
+            yield session.setup();
+
+            //If the table exists, everything's working, but also delete it to clean up
+            let exists = yield* tableExists(sampleTable);
+            if (exists) {
+                yield deleteSchema(sampleTable);
+                done();
+            }
+            else
+                done(new Error("Table not created"));
         })();
     });
 });
